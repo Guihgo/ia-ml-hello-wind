23b0280c32b7abcb16fb66e0f9ff10a893b10a41
Juliana Oliveira <h4bituei.o@gmail.com>
2022-05-15 02:19:34 -0300

    Atualizando o trabalho: variáveis preditoras e variável alvo.

--- work_one.py
+++ work_one.py
@@ -1,16 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import ExtraTreesClassifier
 
 # Exercise 1
 df = pd.read_csv('data/wind_dataset.csv')
-
-# Replace values example
-# file['IND'] = file['IND'].replace(0, 'teste')
-
-# Separate collumn
-# x = file['T.MAX']
-
 
 # Exercise 2
 scatter_matrix(df, diagonal='hist')
@@ -19,9 +14,20 @@ plt.savefig('output/scatter_matrix.jpg')
 # Exercise 3 
 plt.show()
 
-
-
 # Exercise 4: machine learning (KNN OR Random OR Forest OR Decision Tree)... @Ju
 
-#criando conjunto de dados de treino e teste:
-# x_treino, x_teste=
+#Separando as variáveis entre preditoras (x) e variável alvo (y)
+y = df['WIND']
+x = df.drop('WIND', axis= 1)
+
+#Criando os conjuntos de dados de teste e treino
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size =0.3, train_size=0.7)
+
+#Criação do modelo
+modelo = ExtraTreesClassifier()
+modelo.fit(x_treino, y_treino)
+
+#Imprimindo resultados
+resultado = modelo.score(x_teste, y_teste)
+print("Acurácia:", resultado)
+
